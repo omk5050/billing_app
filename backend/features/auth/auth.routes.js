84112129f8dc0ac75d14fdce5ef5dd2b1b5b9ada
Om Kadam <kadamom313@gmail.com>
2026-03-04 01:51:09 +0530

    feat: implement real auth with JWT (Modified with /me)

--- backend/features/auth/auth.routes.js
+++ backend/features/auth/auth.routes.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { register, login } = require('./auth.controller');
+const { protect } = require('../../middleware/auth.middleware');
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/me', protect, (req, res) => {
+  res.json({
+    message: 'Protected route accessed',
+    user: req.user
+  });
+});
 
 module.exports = router;
