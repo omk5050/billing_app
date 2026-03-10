6f545dc3c2f0ea404a4bab034b148b23938f73fb
Om Kadam <kadamom313@gmail.com>
2026-03-10 16:34:41 +0530

    Fixed app.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const app = require("./app");
+
+/* Background Jobs */
 require("./jobs/reminder.job");
 
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const app = require('./app');
-
-dotenv.config();
-
+/* MongoDB Connection */
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("Mongo Error:", err));
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("Mongo Error:", err);
+  });
 
+/* Start Server */
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
