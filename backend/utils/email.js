9d805faa6b492859296332fcba99958bc2c01d98
OM ANIL KADAM <kadamom313@gmail.com>
2026-03-10 17:06:06 +0530

    Refactor email sending function and transport settings

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,22 +1,27 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use STARTTLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
-const sendEmail = async (to, subject, message) => {
+module.exports = async function sendEmail(to, subject, text) {
 
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    from: `"Billing System" <${process.env.EMAIL_USER}>`,
     to,
     subject,
-    text: message
+    text
   });
 
 };
-
-module.exports = sendEmail;
