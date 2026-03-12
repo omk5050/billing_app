388f59af15c9c4223937ee5aa9310742a8f2e3b8
Om Kadam <kadamom313@gmail.com>
2026-03-12 15:48:48 +0530

    fix middleware destructuring

--- backend/features/customer/customer.routes.js
+++ backend/features/customer/customer.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const protect = require("../../middleware/auth.middleware");
+const { protect } = require("../../middleware/auth.middleware");
 const { getCustomers } = require("./customer.controller");
 
 // Debug logs (optional)
