e7a561c75254ddfdd5bb35acb64fcd404d3b0e1b
Om Kadam <kadamom313@gmail.com>
2026-03-12 15:17:40 +0530

    fixed: File path for  Customer routes

--- backend/features/customer/customer.routes.js
+++ backend/features/customer/customer.routes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const { getCustomers } = require("../controllers/customer.controller")
+const { getCustomers } = require("../customer/customer.controller")
 const protect = require("../middleware/auth.middleware")
 
 router.get("/customers", protect, getCustomers)
